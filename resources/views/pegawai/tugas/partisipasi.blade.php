424d1b33a3d520afcaee44396816e78621d3f11d
arieffakhruddin19 <arieffakhruddin19@gmail.com>
2026-07-25 00:16:29 +0700

    Further reduce size of Export Excel button

--- resources/views/pegawai/tugas/partisipasi.blade.php
+++ resources/views/pegawai/tugas/partisipasi.blade.php
@@ -28,8 +28,8 @@
                     </style>
                     <div class="flex flex-col sm:flex-row justify-between w-full gap-4 mb-4">
                         <div class="flex-shrink-0">
-                            <a href="{{ route('tugas.partisipasi.export', request()->query()) }}" class="inline-flex items-center px-3 py-2 bg-green-600 border border-transparent rounded-lg font-semibold text-xs text-white hover:bg-green-700 active:bg-green-900 focus:outline-none focus:border-green-900 focus:ring ring-green-300 disabled:opacity-25 transition ease-in-out duration-150 shadow-sm">
-                                <svg class="w-4 h-4 mr-1.5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg>
+                            <a href="{{ route('tugas.partisipasi.export', request()->query()) }}" class="inline-flex items-center px-2 py-1 bg-green-600 border border-transparent rounded-md font-semibold text-[10px] text-white hover:bg-green-700 active:bg-green-900 focus:outline-none focus:border-green-900 focus:ring ring-green-300 disabled:opacity-25 transition ease-in-out duration-150 shadow-sm">
+                                <svg class="w-3 h-3 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg>
                                 Export Excel
                             </a>
                         </div>
